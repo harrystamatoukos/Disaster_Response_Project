95c1f60a1d7b28807c02f8240dafc61d683317e3
Harry Stamatoukos <h.stamatoukos@gmail.com>
2020-03-28 14:22:32 +0000

    add second graph

--- app/run.py
+++ app/run.py
@@ -7,9 +7,13 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+
+import plotly.graph_objs as goo
+
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
+
+import gc
 
 
 app = Flask(__name__)
@@ -29,10 +33,13 @@ def tokenize(text):
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('StaginMLtable', engine)
+#df = pd.read_sql_table('StaginMLtable', engine)
 
 # load model
 model = joblib.load("../models/classifier.plk")
+print(model)
+
+
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -41,60 +48,63 @@ model = joblib.load("../models/classifier.plk")
 def index():
 
     # extract data needed for visuals
+    df = pd.read_sql_table('StaginMLtable', engine)
 
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
 
-    # data for visualizing category counts.
+    ### data for visualizing category counts.
     label_sums = df.iloc[:, 4:].sum()
     label_names = list(label_sums.index)
 
     # create visuals
-graphs = [
-    {
-        'data': [
-            Bar(
-                x=genre_names,
-                y=genre_counts
-            )
-        ],
+    graphs = [
+        {
+            'data': [
+                goo.Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
 
-        'layout': {
-            'title': 'Distribution of Message Genres',
-            'yaxis': {
-                'title': "Count"
-            },
-            'xaxis': {
-                'title': "Genre"
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+
+        {
+            'data': [
+                goo.Bar(
+                    x=label_names,
+                    y=label_sums,
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+
+                },
             }
         }
-    },
-    {
-        'data': [
-            Bar(
-                x=label_names,
-                y=label_sums,
-            )
-        ],
 
-        'layout': {
-            'title': 'Distribution of categories',
-            'yaxis': {
-                'title': "Count"
-            },
-            'xaxis': {
+    ]
 
-            },
-        }
-    }
-]
-# encode plotly graphs in JSON
-ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
-graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
 
-# render web page with plotly graphs
-return render_template('master.html', ids=ids, graphJSON=graphJSON)
-
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 # web page that handles user query and displays model results
 @app.route('/go')

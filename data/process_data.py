02f66d7fdac09fa758bd28fc0b1bc77c5818058a
Harry Stamatoukos <h.stamatoukos@gmail.com>
2020-03-28 13:26:57 +0000

    Adding function docstring details

--- data/process_data.py
+++ data/process_data.py
@@ -3,24 +3,25 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-
 def load_data(messages_filepath, categories_filepath):
     '''
     INPUT
-    messages_filepath
+    messages_filepath - to import the database
 
     OUTPUT
 
-    blah blah
+    A merged dataframe of the messages and categories datasets
+
 
     '''
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
-    #messages = messages.drop(columns=['original'])
+    # messages = messages.drop(columns=['original'])
 
     df = pd.merge(messages, categories, on='id')
 
     return df
+
 
 def clean_data(df):
     '''
@@ -28,16 +29,14 @@ def clean_data(df):
     df : a merged data frame
 
     OUTPUT
-    df:
-    database_filename:
+    Retursn a cleaned dataframe
 
     '''
-    #split the categories
+    # split the categories
 
-    df_categories = df.categories.str.split(pat = ';', expand = True)
+    df_categories = df.categories.str.split(pat=';', expand=True)
 
-
-    #rename the columns with labels from the first row
+    # rename the columns with labels from the first row
     df_categories.columns = df_categories.loc[0]
 
     df_categories.columns = [w[:-2] for w in df_categories.columns]
@@ -49,20 +48,24 @@ def clean_data(df):
         # convert column from string to numeric
         df_categories[column] = pd.to_numeric(df_categories[column])
 
-    df.drop(columns='categories',inplace=True)
+    df.drop(columns='categories', inplace=True)
 
-    df = pd.concat([df, df_categories], axis = 1, join_axes=[df.index])
+    df = pd.concat([df, df_categories], axis=1, join_axes=[df.index])
 
     df.drop_duplicates(inplace=True)
 
     return df
 
 
+def save_data(df, database_filename, table_name='StaginMLtable'):
+        """
 
-
-
-
-def save_data(df, database_filename, table_name = 'StaginMLtable'):
+    INPUT:
+        df: Dataframe to be saved
+        database_filepath - Filepath used for saving the database
+    OUTPUT:
+        Saves the database
+    """
 
     engine = create_engine('sqlite:///'+database_filename)
     df.to_sql(table_name, engine, index=False)
@@ -82,16 +85,16 @@ def main():
         df = clean_data(df)
 
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath, table_name= 'StaginMLtable')
+        save_data(df, database_filepath, table_name='StaginMLtable')
 
         print('Cleaned data saved to database!')
 
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories '
+              'datasets as the first and second argument respectively, as '
+              'well as the filepath of the database to save the cleaned data '
+              'to as the third argument. \n\nExample: python process_data.py '
+              'disaster_messages.csv disaster_categories.csv '
               'DisasterResponse.db')
 
 
